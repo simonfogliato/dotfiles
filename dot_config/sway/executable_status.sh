be7d383b9b7a3dc842d1888d3f21aa08a2d9943e
Simon Fogliato <simonfogliato@gmail.com>
2024-01-18 10:36:22 -0800

    Added audio sink source control and status.

--- dot_config/sway/executable_status.sh
+++ dot_config/sway/executable_status.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-status_volume="\"volume\": \"`wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/^Volume: //'`\""
+status_audio="\"audio\": \"`wpctl status | grep -m 1 '*' | awk '{print $4}'`\""
+status_sink="\"sink\": \"`wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/^Volume: //'`\""
+status_source="\"source\": \"`wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | sed 's/^Volume: //'`\""
 status_swaync="\"swaync\": `swaync-client -D`"
 status_epoch="\"epoch\": `date +'%s'`"
 status_date="\"date\": \"`date +'%a %d %b %Y %r %Z'`\""
 if [ "$(upower --enumerate | grep -c BAT)" -ne "0" ]; then
 	status_power="\"power\": \"`upower --show-info $(upower --enumerate | grep BAT) | grep percentage | awk '{print $2}'`\""
 	status_brightness="\"brightness\": \"`brightnessctl info | grep 'Current brightness' | awk '{ print $4}' | tr -d '()'`\""
-	echo "{ $status_power, $status_brightness, $status_volume, $status_swaync, $status_epoch, $status_date }"
+	echo "{ $status_power, $status_brightness, $status_audio, $status_sink, $status_source, $status_swaync, $status_epoch, $status_date }"
 else
-	echo "{ $status_volume, $status_swaync, $status_epoch, $status_date }"
+	echo "{ $status_audio, $status_sink, $status_source, $status_swaync, $status_epoch, $status_date }"
 fi
